726f98debae81ccf2a015caa82b1185a8f0fccd1
Isuru Lakmal <96872844+IsuruLakmal2000@users.noreply.github.com>
2025-06-30 21:10:49 +0530

    Update RoleCards.jsx

--- src/components/sections/RoleCards.jsx
+++ src/components/sections/RoleCards.jsx
@@ -10,7 +10,7 @@ import {
   DocumentTextIcon
 } from '@heroicons/react/24/outline';
 
-import 'C:/Users/kavin/OneDrive/Desktop/Talent Hub/talenthub-outsource-comp/src/styles/globals.css';
+import '../styles/globals.css';
 
 const RoleCards = () => {
   const [selectedCategory, setSelectedCategory] = useState('all');
